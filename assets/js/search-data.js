e723a34107662f9815f40993eb46ff9f91e38736
mfaizan-ai <mohammadfaizantanoli@gmail.com>
2026-05-08 16:40:12 +0000

    Deploying to gh-pages from @ mfaizan-ai/mfaizan-ai.github.io@3c39220a69a76c73a0670df2737d0120dc36fb78 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "A selection of research and engineering projects spanning deep learning, medical imaging, and computer vision.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
